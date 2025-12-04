0be8ca49eba6b50d82fe964899e769d73d99509e
piyrin <3308070091@qq.com>
2025-12-04 15:42:05 +0800

    更新config.cpp的功能实现

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -1,4 +1,4 @@
-#include"config.h"
+#include"Config.h"
 #include<fstream>
 #include<sstream>
 //初始化静态指针
@@ -24,7 +24,7 @@ void Config::loadIni(const string &ini_path)
     //打开配置文件
     ifstream file(ini_path);
     //打开失败，使用默认配置
-    if(!file.is_open)
+    if(!file.is_open())
     {
         server_conf.port = 8080;
         server_conf.buffer_size = 4096;
@@ -42,9 +42,52 @@ void Config::loadIni(const string &ini_path)
         //跳过空行和注释行
         if (line.empty() || line[0] == ';')
             continue;
-        if()
+        //识别节
+        if(line[0]=='['&&line.back()==']')
+        {
+            current_section = line.substr(1, line.size() - 2);
+            continue;
+        }
+        //解析键值对
+        size_t eq_pos = line.find('=');
+        if (eq_pos == string ::npos)
+            continue;//没有=跳过
+        
+        //分割key和value，去掉前后空格
+        string key = line.substr(0, eq_pos);
+        string value = line.substr(eq_pos + 1);
+        //去掉key前后空格
+        key.erase(0, key.find_first_not_of(" \t"));
+        key.erase(key.find_last_not_of(" \t")+1);
+        //去掉value前后空格
+        value.erase(0, value.find_first_not_of(" \t"));
+        value.erase(value.find_last_not_of(" \t") + 1);
+        // 去掉value后面‘;’部分
+        size_t comment_pos = value.find(';');
+        if(comment_pos!=string::npos)
+            value = value.substr(0, comment_pos);
+        value.erase(value.find_last_not_of(" \t") + 1);
+        // 根据当前节点填到对应结构体
+        if (current_section == "server")
+        {
+            if(key=="port")
+                server_conf.port = stoi(value);
+            else if(key=="buffer_size")
+                server_conf.buffer_size = stoi(value);
+            else if(key=="backlog")
+                server_conf.backlog = stoi(value);
+        }
+        else if(current_section=="path")
+        {
+            if(key=="view_dir")
+                path_conf.view_dir = value;
+            else if(key=="public_dir")
+                path_conf.public_dir = value;
+            else if(key=="db_path")
+                path_conf.db_path = value;
+        }
     }
-
+    file.close();
 }
 
 ServerConfig Config::getServerConfig() const
