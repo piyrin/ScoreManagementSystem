6cee01b6867f0fc1857653e3612491742fa0af96
piyrin <3308070091@qq.com>
2025-12-04 09:09:35 +0800

    完成配置模块头文件Config.h的编写：声明ServerConfig/PathConfig结构体与Config单例类

---
+++ src/config/config.h
@@ -0,0 +1,43 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include<iostream>
+#include<string>
+using namespace std;
+struct ServerConfig
+{
+    int port=8080;         //监听端口
+    int buffer_size;  //请求缓冲区大小
+    int backlog;      //连接等待队列长度
+};
+struct PathConfig
+{
+    string view_dir;    //页面文件目录
+    string public_dir;  //静态文件目录
+    string db_path;     //数据文件库目录
+};
+class Config
+{
+    private:
+        //私有成员，存储配置的结构体
+        ServerConfig server_conf;
+        PathConfig path_conf;
+        //静态指针（全局唯一实例）
+        static Config *instance;
+        //私有构造函数，禁止外部创建实例
+        Config();
+        //私有函数，加载ini配置文件
+        void loadIni(const string &ini_path = "conf/server.ini");
+
+    public:
+        //避免复制构造和赋值
+        Config(const Config&) = delete;
+        Config &operator=(const Config&) = delete;
+
+        //静态函数，获取单例实例
+        static Config *getInstance();
+
+        //公有函数，获取配置（供其他模块使用）
+        ServerConfig getServerConfig() const;
+        PathConfig getPathConfig() const;
+};
+#endif 
