e41d9269f514b81d64b74527787ce1412cfd5222
piyrin <3308070091@qq.com>
2025-12-07 22:43:03 +0800

    Add SQLite library and update build configuration

--- compile.bat
+++ compile.bat
@@ -5,7 +5,7 @@ set "BIN_DIR=bin"
 
 if not exist %BIN_DIR% mkdir %BIN_DIR%
 
-g++ %SRC_DIR%/*.cpp %SRC_DIR%/config/*.cpp %SRC_DIR%/utils/*.cpp %SRC_DIR%/network/*.cpp %SRC_DIR%/dao/*.cpp %SRC_DIR%/service/*.cpp %SRC_DIR%/core/Model/*.cpp %SRC_DIR%/core/Controller/*.cpp -o %BIN_DIR%/score_system.exe -lws2_32 -lsqlite3 -finput-charset=UTF-8 -fexec-charset=UTF-8
+g++ main.cpp %SRC_DIR%/config/*.cpp %SRC_DIR%/utils/*.cpp %SRC_DIR%/network/*.cpp %SRC_DIR%/dao/*.cpp %SRC_DIR%/service/*.cpp %SRC_DIR%/core/Model/*.cpp %SRC_DIR%/core/Controller/*.cpp lib/sqlite/sqlite3.c -o %BIN_DIR%/score_system.exe -Ilib/sqlite -lws2_32 -finput-charset=UTF-8 -fexec-charset=UTF-8
 
 if %errorlevel% equ 0 (
     echo [成功] 编译完成！输出路径：%BIN_DIR%/score_system.exe
